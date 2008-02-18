a839a3f5c0091d275c64159ded0e12d36b5e2bdb
Cameron Dale <camrdale@gmail.com>
2008-02-17 20:30:06 -0800

    Fix up some errors in the packaging.

--- setup.py
+++ setup.py
@@ -1,57 +1,17 @@
 #!/usr/bin/env python
 
-try:
-    import distutils.core
-    import distutils.command.build_ext
-except ImportError:
-    raise SystemExit, """\
-You don't have the python development modules installed.  
+import sys
+from distutils.core import setup
 
-If you have Debian you can install it by running
-    apt-get install python-dev
+setup(
+    name = "apt-dht",
+    version = "0.0.0",
+    author = "Cameron Dale",
+    author_email = "<camrdale@gmail.com>",
+    url = "http://www.camrdale.org/apt-dht.html",
+    license = "GPL",
 
-If you have RedHat and know how to install this from an RPM please
-email us so we can put instructions here.
-"""
+    packages = ["apt_dht", "apt_dht_Khashmir"],
 
-try:
-    import twisted
-except ImportError:
-    raise SystemExit, """\
-You don't have Twisted installed.
-
-Twisted can be downloaded from 
-    http://twistedmatrix.com/products/download
-
-Anything later that version 1.0.3 should work
-"""
-
-try:
-    import pysqlite2
-except ImportError:
-    raise SystemExit, """\
-You don't have PySQLite installed.
-
-PySQLite can be downloaded from 
-    http://sourceforge.net/project/showfiles.php?group_id=54058&release_id=139482
-"""
-
-setup_args = {
-    'name': 'khashmir',
-    'author': 'Andrew Loewenstern',
-    'author_email': 'burris@users.sourceforge.net',
-    'licence': 'MIT',
-    'package_dir': {'khashmir': '.'},
-    'packages': [
-        'khashmir', 
-    ],
-}
-
-if hasattr(distutils.dist.DistributionMetadata, 'get_keywords'):
-    setup_args['keywords'] = "internet tcp p2p"
-
-if hasattr(distutils.dist.DistributionMetadata, 'get_platforms'):
-    setup_args['platforms'] = "win32 posix"
-
-if __name__ == '__main__':
-    apply(distutils.core.setup, (), setup_args)
+    scripts = ['apt-dht.py']
+    )
