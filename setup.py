e1b99a7d84fce8e84e3a6c30fd40db18b4f0b5ad
Cameron Dale <camrdale@gmail.com>
2008-02-21 11:10:38 -0800

    Automatically set the version in setup.py from the changelog.

--- setup.py
+++ setup.py
@@ -3,9 +3,17 @@
 import sys
 from distutils.core import setup
 
+from debian_bundle import changelog
+
+f = open('debian/changelog')
+data = f.read()
+f.close()
+c = changelog.Changelog(file=data, max_blocks=1)
+del data
+
 setup(
     name = "apt-dht",
-    version = "0.0.0",
+    version = c.full_version,
     author = "Cameron Dale",
     author_email = "<camrdale@gmail.com>",
     url = "http://www.camrdale.org/apt-dht.html",
