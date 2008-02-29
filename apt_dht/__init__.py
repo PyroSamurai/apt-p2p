20b6eecbd33df2f174a1a09c70d4cf89813f3a8f
Cameron Dale <camrdale@gmail.com>
2008-02-29 13:41:50 -0800

    Document the apt_dht module's __init__ file.

--- apt_dht/__init__.py
+++ apt_dht/__init__.py
@@ -7,7 +7,7 @@ Diagram of the interaction between the given modules::
   |     AptDHT    |    |               DHT                 |    |  Internet
   |               |--->|join                            DHT|----|--\    
   |               |--->|loadConfig                         |    |  | Another
-  |               |--->|getValue                           |    |  | Peer
+  |               |--->|getValue                           |    |  | Node
   |               |--->|storeValue                      DHT|<---|--/
   |               |--->|leave                              |    |
   |               |    +-----------------------------------+    |
@@ -16,26 +16,25 @@ Diagram of the interaction between the given modules::
   |               |--->|get          |--->|get         HTTP|----|---> Mirror
   |               |    |             |--->|getRange        |    |
   |               |--->|close        |--->|close       HTTP|----|--\       
-  |               |    +-------------+    +----------------+    |  |
-  |               |    +-----------------------------------+    |  | Another
-  |               |    |           HTTPServer              |    |  | Peer
-  |               |--->|getHTTPFactory                 HTTP|<---|--/
+  |               |    +-------------+    +----------------+    |  | Another
+  |               |    +-----------------------------------+    |  | Peer
+  |               |    |           HTTPServer          HTTP|<---|--/   
+  |               |--->|getHTTPFactory                     |    +-------------
   |check_freshness|<---|                                   |    +-------------
-  |       get_resp|<---|                                   |    +-------------
-  |          /----|--->|setDirectories                 HTTP|<---|HTTP Request
-  |          |    |    +-----------------------------------+    | 
-  |          |    |    +---------------+    +--------------+    | Local Net
-  |          |    |    | CacheManager  |    | ProxyFile-   |    | (apt)
-  |          |    |--->|scanDirectories|    | Stream*      |    |
-  | setDirectories|<---|               |--->|__init__  HTTP|--->|HTTP Response
-  |               |--->|save_file      |    |              |    +-------------
+  |       get_resp|<---|                               HTTP|<---|HTTP Request
+  |               |    +-----------------------------------+    | 
+  |               |    +---------------+    +--------------+    | Local Net
+  |               |    | CacheManager  |    | ProxyFile-   |    | (apt)
+  |               |--->|scanDirectories|    | Stream*      |    |
+  |               |--->|save_file      |--->|__init__  HTTP|--->|HTTP Response
   |               |--->|save_error     |    |              |    +-------------
+  |               |    |               |    |              |    +-------------
   |new_cached_file|<---|               |    |          file|--->|write file
   |               |    +---------------+    +--------------+    |
   |               |    +---------------+    +--------------+    | Filesystem
   |               |    | MirrorManager |    | AptPackages* |    |
-  |               |--->|updatedFile    |--->|file_updated  |--->|write file
-  |               |--->|findHash       |--->|findHash      |    |
+  |               |--->|updatedFile    |--->|file_updated  |    | 
+  |               |--->|findHash       |--->|findHash  file|<---|read file
   +---------------+    +---------------+    +--------------+    +-------------
 
 """
