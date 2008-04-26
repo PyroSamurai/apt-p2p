2ddf7b9797406505aa3ee1c40de12843b711eefd
Cameron Dale <camrdale@gmail.com>
2008-04-25 18:57:05 -0700

    Remove the changelog parsing from setup.py and just hardcode the version.

--- setup.py
+++ setup.py
@@ -1,19 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 import sys
 from distutils.core import setup
 
-from debian_bundle import changelog
-
-f = open('debian/changelog')
-data = f.read()
-f.close()
-c = changelog.Changelog(file=data, max_blocks=1)
-del data
-
 setup(
     name = "apt-p2p",
-    version = c.full_version,
+    version = "0.1.0",
     author = "Cameron Dale",
     author_email = "<camrdale@gmail.com>",
     url = "http://www.camrdale.org/apt-p2p.html",
