3728713538ebe29ed01d9ab40cf582b3466b8a7a
Cameron Dale <camrdale@gmail.com>
2008-06-21 23:31:48 -0700

    New changelog entry and version update for relase.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name = "apt-p2p",
-    version = "0.1.4",
+    version = "0.1.5",
     author = "Cameron Dale",
     author_email = "<camrdale@gmail.com>",
     url = "http://www.camrdale.org/apt-p2p.html",
